86b67f21400f2c024bdf08c0a5f459d089c40f3c
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-08-16 15:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let cars=document.getElementById("colorSelect");
+  let clrs=document.querySelector("input");
+  clrs.addEventListener("click", () =>{
+    cars.remove(cars.selectedIndex);
+  })
